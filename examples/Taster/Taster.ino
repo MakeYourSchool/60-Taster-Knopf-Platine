d0d0954fa1dc077d5f9a58c858725889b82df9c5
MakeYourSchool <paul@creators-collective.com>
2020-03-21 22:02:13 +0100

    edit

---
+++ examples/Taster/Taster.ino
@@ -0,0 +1,30 @@
+//This example code was originally published on the official Seeed Studio Wiki Website: http://wiki.seeedstudio.com/Grove-Button/
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+    // initialize the LED pin as an output:
+    pinMode(ledPin, OUTPUT);
+    // initialize the pushbutton pin as an input:
+    pinMode(buttonPin, INPUT);
+}
+
+void loop(){
+    // read the state of the pushbutton value:
+    buttonState = digitalRead(buttonPin);
+
+    // check if the pushbutton is pressed.
+    // if it is, the buttonState is HIGH:
+    if (buttonState == HIGH) {
+        // turn LED on:
+        digitalWrite(ledPin, HIGH);
+    }
+    else {
+        // turn LED off:
+        digitalWrite(ledPin, LOW);
+    }
+}
